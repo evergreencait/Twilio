d7168e7cc6de7413896bf0c64c47ceb796c96778
Alexandra Holcombe, Caitlin Hines and Katy Daviscourt <alexandra+caitlin+katy@>
2017-05-09 14:12:35 -0700

    cat facts api working

--- src/Texter/Models/Message.cs
+++ src/Texter/Models/Message.cs
@@ -30,13 +30,30 @@ namespace Texter.Models
             return messageList;
         }
 
+        public static List<String> GetQuote()
+        {
+            var client = new RestClient("http://catfacts-api.appspot.com/api/facts");
+            var request = new RestRequest("", Method.GET);
+            Console.WriteLine(request);
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            Console.WriteLine(jsonResponse);
+            var messageList = JsonConvert.DeserializeObject<List<String>>(jsonResponse["facts"].ToString());
+            return messageList;
+        }
+
         public void Send()
         {
             var client = new RestClient("https://api.twilio.com/2010-04-01");
             var request = new RestRequest("Accounts/" + EnvironmentVariables.AccountSid + "/Messages", Method.POST);
+            var quote = GetQuote();
             request.AddParameter("To", To.ToString());
             request.AddParameter("From", From.ToString());
-            request.AddParameter("Body", Body.ToString());
+            request.AddParameter("Body", Body.ToString() + " " + quote[0]);
             client.Authenticator = new HttpBasicAuthenticator(EnvironmentVariables.AccountSid, EnvironmentVariables.AuthToken);
             client.ExecuteAsync(request, response => {
                 Console.WriteLine(response.Content);
